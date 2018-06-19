175a134654b4fbe8c499451b15624b47014f0354
Leon Mak <leonmakansheng@gmail.com>
2018-06-18 22:54:47 -0700

    Add error handlers

--- pi_server.py
+++ pi_server.py
@@ -6,45 +6,45 @@ import RPi.GPIO as GPIO
 
 
 class DoorCommand(IntEnum):
-    UP = 1
-    DOWN = 2
+    DOWN = 1
+    UP = 2
 
 
 def main():
     servo = Servomotor()
     host = ''
     port = 21567
-    buffer_size = 1024
+    buffer_size = 1  # read 1 byte max at a time
     address = (host, port)
     tcp_ser_sock = socket(AF_INET, SOCK_STREAM)
-    tcp_ser_sock.bind(address)
-    tcp_ser_sock.listen(5)
-
-    print('Waiting for connection')
-    tcp_cli_sock, addr = tcp_ser_sock.accept()
-    print('...connected from :', addr)
 
     try:
+        tcp_ser_sock.bind(address)
+        tcp_ser_sock.listen(5)
+        print('Waiting for connection')
+        tcp_cli_sock, addr = tcp_ser_sock.accept()
+        print('...connected from :', addr)
         while True:
-            sleep(3)
             data = tcp_cli_sock.recv(buffer_size)
+            print('read ' + data)
             if not data:
                 break
             try:
-                door_cmd = DoorCommand(data)
-            except KeyError:
+                door_cmd = DoorCommand(int(data))  # 1 / 2
+            except (KeyError, ValueError):
+                print('Data invalid: ' + data)
                 break
             else:
                 if door_cmd == DoorCommand.UP:
                     servo.ServoUp()
-                    print('Increased to: ', servo.cur_X)
                 if door_cmd == DoorCommand.DOWN:
                     servo.ServoDown()
-                    print('Decreased to: ', servo.cur_X)
-    except KeyboardInterrupt:
-        servo.close()
-        GPIO.cleanup()
+    except Exception as e:
+        print(e)
+    finally:
+        print('cleaning up..')
         tcp_ser_sock.close()
+        servo.close()
 
 
 if __name__ == '__main__':
--- servomotor.py
+++ servomotor.py
@@ -3,31 +3,41 @@ import time
 
 
 class Servomotor:
+    """Precise controlled motor
+    Start pwm with Duty Cycle is 2% --> Pulse with = 2%*20ms = 0.4ms
+    Servo cannot turn on and off fast enough. Longer width, Higher Perceived V.
+    Create PWM on pin 11, motor frequency is 50Hz --> period = 20ms
+    1.0ms = CCW = 5.0 dc
+    1.5ms = --  = 7.5 dc
+    2.0ms = CW  = 2/20 = 10% duty cycle
+    Motors map duration to direction / position (degrees). This uses direction.
+    """
     def __init__(self):
+        self.pin = 11
+        self.freq = 50
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11, GPIO.OUT)
-        # start pwm with Duty Cycle is 2% --> Pulse with = 2%*20ms = 0.4ms
-        # Create PWM on pin 11 with frequency 50Hz --> period 20ms
-        self.servo = GPIO.PWM(11, 50)
-        self.servo.start(2.5)
-        self.servo.ChangeDutyCycle(2.5)
-        self.cur_X = 0  # initial value of servo motor
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pin, self.freq)
+        self.position = {
+            'left': 6,
+            'right': 9,
+        }
+        self.pwm.start(2)
 
     def ServoUp(self):
-        self.cur_X += 2.5
-        if self.cur_X > 12:
-            self.cur_X = 12.5
-        self.servo.ChangeDutyCycle(self.cur_X)
+        dc = self.position['left']
+        print('start: ' + str(dc))
+        self.pwm.ChangeDutyCycle(dc)
         time.sleep(1)
 
     def ServoDown(self):
-        self.cur_X -= 2.5
-        if self.cur_X < 2.5:
-            self.cur_X = 2.5
-            self.servo.ChangeDutyCycle(self.cur_X)
+        dc = self.position['right']
+        print('start: ' + str(dc))
+        self.pwm.ChangeDutyCycle(dc)
         time.sleep(1)
 
     def close(self):
-        self.servo.stop()
+        self.pwm.stop()
+        GPIO.cleanup()
 
 
